524ab552433f3ba8cb8d10e35ec7b5c1681b6c77
shadowforce78 <planque.adam@gmail.com>
2024-12-09 11:50:16 +0100

    Update game plateau layout in main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-print('BomberMan')
+# CCCCCCCCC
+# C   P   C
+# CMCMCMCMC
+# CMMMM E C
+# CCCCCCCCC
 
 plateau = [
-    ['C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C'],
+    ["C", "C", "C", "C", "C", "C", "C", "C", "C"],
+    ["C", " ", " ", " ", "P", " ", " ", " ", "C"],
+    ["C", "M", "C", "M", "C", "M", "C", "M", "C"],
+    ["C", "M", "M", "M", "M", " ", "E", " ", "C"],
+    ["C", "C", "C", "C", "C", "C", "C", "C", "C"],
 ]
+
+for i in range(len(plateau)):
+    for j in range(len(plateau[i])):
+        print(plateau[i][j], end="")
+    print()
